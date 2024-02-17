81595ec20dde7701d4e7fdcfbfbda921a982e61e
crltsnch <crltsnch@myuax.com>
2024-02-17 13:12:23 +0100

    25 mejor valorados

--- precios_mercado.py
+++ precios_mercado.py
@@ -24,29 +24,34 @@ if response.status_code == 200:
 
     # Verificar si se encontró la tabla
     if tabla:
-        # Obtener todas las filas y celdas de la tabla
+        # Obtener todas las filas de la tabla
         rows = tabla.find_all('tr')
 
         # Crear una lista para almacenar las filas de datos
         tabla_data = []
 
-        # Recorrer cada fila y extraer el texto de las celdas
         for i, row in enumerate(rows):
-            # Omitir la primera iteración (i == 0) para evitar la primera fila de nombres de columnas
+            # Omitir la fila de encabezados
             if i == 0:
                 continue
-            cells = row.find_all(['td', 'th'])
-            row_data = [cell.get_text(strip=True) for cell in cells]
 
-            tabla_data.append(row_data)
-            print(tabla_data)
-
-        '''# Especificar el nombre del archivo CSV donde guardar la tabla
-        file_name = 'transfermarkt_table.csv'
+            # Agregar solo las filas cuyos índices son múltiplos de 3 (1, 4, 7, 10, etc.)
+            if i % 3 == 1:
+                cells = row.find_all(['td', 'th'])
+                row_data = [cell.get_text(strip=True) for cell in cells]
+                row_data.pop(1)
+                tabla_data.append(row_data)
+    
+        
+    #print(tabla_data)
+        header = ['#', 'null', 'jugador', 'posicion', 'edad', 'null', 'null', 'valor']
+        tabla_data.insert(0, header)
+    # Especificar el nombre del archivo CSV donde guardar la tabla
+        file_name = 'valor_jugadores.csv'
         with open(file_name, 'a', newline='', encoding='utf-8') as f:
             writer = csv.writer(f)
             writer.writerows(tabla_data)
-        print(f'Se guardó la tabla en {file_name}')'''
+        print(f'Se guardó la tabla en {file_name}')
 
     else:
         print('No se encontró la tabla')
