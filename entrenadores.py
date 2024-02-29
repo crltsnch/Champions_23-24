3cceadbbd9ed2a338da638ce238d316c691f268a
crltsnch <crltsnch@myuax.com>
2024-02-29 09:19:48 +0100

    datos entrenadores

--- entrenadores.py
+++ entrenadores.py
@@ -5,64 +5,38 @@ from tabulate import tabulate
 from urllib.parse import urlparse, unquote
 
 # Lista de URLs de equipos
-url = '''https://www.bdfutbol.com/es/t/trcompCHA.html?p=coaches&t=T'''
+url = '''https://www.bdfutbol.com/es/t/trcompCHA.html?p=coaches'''
 
-  # Realizamos la petición a la web
-req = requests.get(url)
-# Extraemos el HTML
-html = req.text
+# Realizar la solicitud GET y crear el objeto BeautifulSoup
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
 
-# Obtener el path de la URL y dividirlo en partes usando '/'
-path_parts = urlparse(url).path.split('/')
+#Buscar la tabla que corresponde a la clase "wikitable" y estilo "text-align:enter"
+tabla = soup.find('div', {'style': 'overflow: auto', 'class': 'w-100'})
 
-# Buscar la parte que contiene el nombre del equipo
-nombre_equipo_part = next((part for part in reversed(path_parts) if part), None)
+#Verificamos si esta
+if tabla:
+    #Obtener todas las filas y celdas de la tabla
+	rows = tabla.find_all('tr')
 
-# Decodificar el nombre del equipo (reemplazar guiones con espacios)
-nombre_equipo = unquote(nombre_equipo_part).replace('-', ' ')
+	#Crear una lista para almacenas las filas de datos
+	tabla_data = []
 
-# Validar si el nombre del equipo se obtuvo correctamente
-if not nombre_equipo:
-    print(f"No se pudo obtener el nombre del equipo del URL: {url}")
-    exit()
+	#Recorrer cada fila y extraer el texto de las celdas
+	for i, row in enumerate(rows):
+		# Omitir la primera iteración (i == 0) para evitar la primera fila de nombres de columnas
+		if i == 0:
+			continue
+		cells = row.find_all(['td', 'th'])
+		row_data = [cell.get_text(strip=True) for cell in cells]
+			
+		tabla_data.append(row_data)
+		print(tabla_data)
 
-# Verificar si la parte "Estadisticas-de-" está presente y quitarla del nombre del equipo
-if "Estadisticas de " in nombre_equipo:
-    nombre_equipo = nombre_equipo.replace("Estadisticas de ", "")
+'''	#Especicificar nombre del csv donde guardar la tabla
+	file_name = 'data/octavos.csv'
+	with open(file_name, 'a', newline='', encoding='utf-8') as f:
+		writer = csv.writer(f)
+		writer.writerows(tabla_data)
+	print(f'Se guardó la tabla en {file_name}')'''
 
-print(f"\nProcesando datos para: {nombre_equipo}")
-
-# Objeto BeautifulSoup
-soup = BeautifulSoup(html, 'html.parser')
-
-# Buscar en el HTML el elemento que se quiere
-jugadores_data = soup.find('table', {'class': 'stats_table', 'id': 'stats_standard_13'})
-
-# Verificar si se encontró la tabla
-if jugadores_data:
-    filas = jugadores_data.find_all('tr')
-    datos = []
-
-    # Iterar sobre las filas y obtener los datos de cada celda
-    for idx, fila in enumerate(filas):
-        # Obtener las celdas de cada fila
-        celdas = fila.find_all(['th', 'td'])
-        datos_fila = [celda.get_text(strip=True) for celda in celdas]
-        if idx == 0:
-            continue  # Saltar la primera fila (encabezados)
-        elif datos_fila:
-            datos_fila.append(nombre_equipo)
-            datos.append(datos_fila)
-
-    # Imprimir la tabla utilizando tabulate
-    tabla = tabulate(datos[1:], headers=datos[0], tablefmt='fancy_grid')
-    print(tabla)
-
-    # Guardar los datos en un archivo CSV
-    ruta_csv = 'data/jugadores.csv'
-    with open(ruta_csv, 'a', newline='', encoding='utf-8') as archivo_csv:
-        escritor_csv = csv.writer(archivo_csv)
-        escritor_csv.writerows(datos)
-
-else:
-    print("No se encontró la tabla con las clases e ID especificados.")
