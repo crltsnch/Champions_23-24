ff1322694ad2b21b7801dc57aa5df7f3e26821bd
crltsnch <crltsnch@myuax.com>
2024-03-06 21:44:07 +0100

    jugadores

--- get/jugador.py
+++ get/jugador.py
@@ -3,44 +3,30 @@ import requests
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 from urllib.parse import urlparse, unquote
-import time
 
-#Lista de temporadas
-temporadas = [
-    '1992-1993', '1993-1994', '1994-1995', '1995-1996', '1996-1997',
-    '1997-1998', '1998-1999', '1999-2000', '2000-2001', '2001-2002',
-    '2002-2003', '2003-2004', '2004-2005', '2005-2006', '2006-2007',
-    '2007-2008', '2008-2009', '2009-2010', '2010-2011', '2011-2012',
-    '2012-2013', '2013-2014', '2014-2015', '2015-2016', '2016-2017']
-
-#A partir del 16-17 la tabla cambia
-'''    '2017-2018', '2018-2019', '2019-2020', '2020-2021', '2021-2022',
-    '2022-2023', '2023-2024'
-]
-'''
-
-
+# Lista de temporadas
+temporadas = ['2022-2023']
 
 resultados = []
 headers = ['#', 'Jugador', 'País', 'Posc', 'Equipo', 'Edad', 'Nacimiento', 'PJ', 'Titular', 'Mín', '90 s', 'Gls.', 'Ass', 'G+A', 'G-TP', 'TP', 'TPint', 'TA', 'TR', 'Gls.90', 'Ast90', 'G+A90', 'G-TP90', 'G+A-TP90', 'Partidos']
 
-
 for temporada in temporadas:
     url = f'https://fbref.com/es/comps/8/{temporada}/stats/Estadisticas-{temporada}-Champions-League'
-    # Realizar la solicitud GET y crear el objeto BeautifulSoup
     
+    # Realizar la solicitud GET y crear el objeto BeautifulSoup
     response = requests.get(url)
     
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # Buscar la tabla que corresponde a la clase "wikitable" y estilo "text-align:enter"
-        tabla = soup.find('table', {'class':'min_width sortable stats_table min_width shade_zero'})
+        tabla = soup.find_all('table', class_='min_width')
+        print(tabla)
 
         # Verificar si se encontró la tabla
         if tabla:
             # Obtener todas las filas y celdas de la tabla
-            rows = tabla.find_all('tr')
+            rows = tabla[1].find_all('tr')
 
             # Crear una lista para almacenar las filas de datos
             tabla_data = []
